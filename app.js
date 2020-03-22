2c114d1ffefe2cacd4d8b50bc87a715782ef6cdd
Bolaji Adetoye <badetoye@yahoo.com>
2020-03-22 22:12:11 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+let name = "Bolaji Adetoye";
+const courses = ["HTML","CSS","JavaScript","C#","Kotlin"];
+console.log("I am " + name + ".\nThese are my chosen courses:\n" + courses);
+console.log("Print Odd Numbers to the Console:")
+    if (courses.length%2 !==0) {
+        for (let j=1; j<=200; j+=2) {
+            console.log(j);  
+            
+        }
+      }
